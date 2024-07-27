bb2fff706eebb18ee3feb422338ad44a0456def9
Nada Elgarf <nadaelgarf477@gmail.com>
2024-07-27 22:57:27 +0300

    Initial commit

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class WeatherService {
 
-  private apiUrl = 'http://api.weatherapi.com/v1/current.json';
+  private apiUrl = 'https://api.weatherapi.com/v1/current.json';
   private apiKey = '8d881f6b7072400ca21181505242707';
 
   constructor(private http:HttpClient) { }
